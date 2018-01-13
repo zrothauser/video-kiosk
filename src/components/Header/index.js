18fa1b93fa9e7c6974ef3fb3421567092816b0fc
Zack Rothauser <zrothauser@gmail.com>
2018-01-13 15:41:49 -0600

    Add styling for the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Styles
+import './index.css';
+
 const Header = () => (
-  <header>
-    <Link to="/">
-            Clyfford Still Museum Media Channel
-    </Link>
-    <Link to="/">
-            Video Index
-    </Link>
+  <header className="b-header">
+    <h1 className="b-header__title">
+      <Link
+        to="/"
+        className="b-header__link"
+      >
+        Clyfford Still Museum Media Channel
+      </Link>
+    </h1>
+
+    <span className="b-header__index">
+      <button
+        onClick={() => console.log('clicked Video Index')}
+        className="b-header__link"
+      >
+        Video Index
+      </button>
+    </span>
   </header>
 );
 
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -10,10 +10,13 @@ import VideoScreen from '../VideoScreen';
 // Components
 import Header from '../../components/Header';
 
+// Styles
+import './index.css';
+
 export const App = () => (
   <div>
     <Header />
-    <main>
+    <main className="b-main">
       <Route exact path="/" component={HomeScreen} />
       <Route path="/category/:slug" component={CategoryScreen} />
       <Route path="/video/:id" component={VideoScreen} />
