4a7b8ba51b8da04fcc4d9105b1eaffc51b42c7a8
Zack Rothauser <zrothauser@gmail.com>
2018-07-14 17:59:14 -0500

    Clean up rendering background video

--- src/containers/HomeScreen/index.js
+++ src/containers/HomeScreen/index.js
@@ -57,66 +57,19 @@ export class HomeScreen extends React.Component {
       backgroundVideoID,
     } = this.props;
 
-    // Set up background video sizing
-    // TODO this should be cleaner
-    const ratioW = (1920 / 1080) * 100;
-    const ratioH = (1080 / 1920) * 100;
-    let fillScreen = {};
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-
-    if ((windowWidth / windowHeight) < 1920 / 1080) {
-      fillScreen = [
-        'height',
-        '100vh',
-        'width',
-        `${ratioW}vh`,
-        'left',
-        `${(windowWidth - ((ratioW * windowHeight) / 100)) / 2}`,
-      ];
-    } else {
-      fillScreen = [
-        'width',
-        '100vw',
-        'height',
-        `${ratioH}vw`,
-        'top',
-        `${(windowHeight - ((ratioH * windowWidth) / 100)) / 2}`,
-      ];
-    }
-
-    const backgroundVideoStyles = {
-      position: 'absolute',
-      top: '0 !important',
-      zIndex: '3',
-      [fillScreen[0]]: fillScreen[1],
-      [fillScreen[2]]: fillScreen[3],
-      [fillScreen[4]]: parseInt(fillScreen[5], 10),
-    };
-
-    // Determine what type of content we should show
-    let pageContent;
-
-    if (isErrored && !isLoading) {
-      pageContent = this.renderError();
-    } else {
-      pageContent = this.renderCategories();
-    }
-
     // And build the page markup
     return (
       <div className="b-homescreen">
         <div className="b-homescreen__wrapper">
           {backgroundVideoID &&
             <iframe
-              className="backgroundvideo"
+              className="b-homescreen__background-video"
               title="BackgroundVideo"
               src={`https://player.vimeo.com/video/${backgroundVideoID}?api=1&background=1`}
-              style={backgroundVideoStyles}
               frameBorder="0"
             />
           }
-          {pageContent}
+          {(isErrored && !isLoading) ? this.renderError() : this.renderCategories()}
         </div>
         <ScrollLock />
       </div>
